75120472398cae976b710f06ff2c57e74ddb5e73
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2024-07-16 22:35:35 +0900

    add kdb schema

--- src/content/kdb-schema.ts
+++ src/content/kdb-schema.ts
@@ -37,6 +37,21 @@ const KdBSchema = z.object({
     ),
 });
 
+const getInstructionalTypeFromCode = (code: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8"): string => {
+    return {
+        '0': 'その他',
+        '1': '講義',
+        '2': '演習',
+        '3': '実習･実験･実技',
+        '4': '講義及び演習',
+        '5': '講義及び実習･実験･実技',
+        '6': '演習及び実習･実験･実技',
+        '7': '講義、演習及び実習･実験･実技',
+        '8': '卒業論文･卒業研究等',
+
+    }[ code ];
+};
+
 const parsedKdb: KdbType = KdBSchema.parse(kdbJson);
 
 export const kdbData = {
@@ -44,7 +59,7 @@ export const kdbData = {
     subject: parsedKdb.subject.map(v => ({
         number: v[0],
         name: v[1],
-        instructionalType: v[2],
+        instructionalType: getInstructionalTypeFromCode(v[2]),
         credits: v[3],
         standardRegistrationYear: v[4],
         term: v[5],
