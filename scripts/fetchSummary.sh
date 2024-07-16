cc8298823eb5faa4f811efedafc5a08a841ca90e
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2024-07-16 21:28:32 +0900

    add: kdbとsummaryを追加した

---
+++ scripts/fetchSummary.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -Lo src/content/class/summary.json "https://r2.bridge2024.tsukuba.dev/2023/contents/summary_other.json"
+curl -Lo src/content/class/summary_taiiku.json "https://r2.bridge2024.tsukuba.dev/2023/contents/summary_taiiku.json"
